dd335bd60af793ba81e9c85e7e04211e8c6653be
Angelica <Angelica137@users.noreply.github.com>
2021-08-24 21:26:57 +0100

    create LinkedList class

--- scripts/Node.py
+++ scripts/Node.py
@@ -1,7 +1,20 @@
-from typing import ValuesView
-
-
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+class LinkedList:
+    def __init__(self, head):
+        self.head = Node(head)
+
+    def print_list(self, head):
+        current_node = self.head
+        while current_node != None:
+            print(current_node.value)
+            current_node = current_node.next
+
+
+linked_list = LinkedList(4)
+linked_list.head.next = Node(3)
+linked_list.print_list(linked_list.head.value)
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,4 +1,4 @@
-from scripts.Node import Node
+from scripts.Node import Node, LinkedList
 
 
 def test_node_returns_head_value_2():
@@ -10,3 +10,11 @@ def test_node_returns_next_1():
     head = Node(2)
     head.next = Node(1)
     assert head.next.value == 1
+
+
+'''
+def test_node_return_LinkedList():
+    linked_list = LinkedList(4)
+    linked_list.head.next = Node(3)
+    assert linked_list.print_list(linked_list.head.value) == [4, 3]
+'''
